53dd939e8b1ad6b86532316cc9289d328e9c013f
aoncivicdev <aoncivic@gmail.com>
2025-04-18 13:04:18 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+const CHANNEL_ACCESS_TOKEN = 'ใส่ ACCESS TOKEN ของ LINE BOT ที่นี่';
+
+app.post('/webhook', async (req, res) => {
+  const events = req.body.events;
+  for (let event of events) {
+    if (event.type === 'message' && event.message.type === 'text') {
+      const replyToken = event.replyToken;
+      const userMessage = event.message.text;
+
+      await axios.post('https://api.line.me/v2/bot/message/reply', {
+        replyToken: replyToken,
+        messages: [{ type: 'text', text: `อ้นส่งว่า: ${userMessage}` }]
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}`
+        }
+      });
+    }
+  }
+
+  res.sendStatus(200);
+});
+
+app.get('/', (req, res) => {
+  res.send('LINE Webhook ของอ้นพร้อมแล้วจ้า!');
+});
+
+app.listen(3000, () => {
+  console.log('Bot is running on port 3000');
+});
